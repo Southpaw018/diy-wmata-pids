e9035a8c0eb47cd181a009d9fb0d1b5d35b70c0b
Jim Drey <moof@moofit.com>
2014-04-06 20:37:20 -0400

    Indentation was spaces in wx function

--- resources/pids.js
+++ resources/pids.js
@@ -51,19 +51,20 @@ function updateClock() {
 }
 
 function updateWeather() {
-    var url = "http://api.openweathermap.org/data/2.5/weather?id=4370890";
+	var url = "http://api.openweathermap.org/data/2.5/weather?id=4370890";
 
-    $.getJSON(url, function(wxData) {
-        var tempK = wxData.main.temp;
-        var tempF = (tempK - 273) * 1.8 + 32;
-        var tempStr = Math.round(tempF);
+	$.getJSON(url, function(wxData) {
+		var tempK = wxData.main.temp;
+		var tempF = (tempK - 273) * 1.8 + 32;
+		var tempStr = Math.round(tempF);
 
-        var cond = wxData.weather[0].description;
-        var condStr = cond.charAt(0).toUpperCase() + cond.slice(1);
+		var cond = wxData.weather[0].description;
+		var condStr = cond.charAt(0).toUpperCase() + cond.slice(1);
 
-        $('#wx span.obs').html(condStr);
-        $('#wx span.temp').html(tempStr);
-    });
+		$('#wx span.obs').html(condStr);
+		$('#wx span.temp').html(tempStr);
+	});
+
 }
 
 
